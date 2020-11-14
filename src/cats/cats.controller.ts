1067b605b7e28ca3f5b50c5e38f7219d81ca1fbd
chylomicronman <bmccann36@gmail.com>
2020-11-14 11:20:16 -0500

    initial commit, at providers section of nest.js intro docs

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    console.log(createCatDto);
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
